5ef52a2e251c81afe197152e54d47d34b3fa448b
wiredmartian <solomzi.jikani@gmail.com>
2024-01-04 22:16:18 +0200

    initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module glox
+
+go 1.21.5
---
+++ scanner/token.go
@@ -0,0 +1,14 @@
+package scanner
+
+import "fmt"
+
+type Token struct {
+	tokenType TokenType
+	lexeme    string
+	literal   interface{}
+	lint      int
+}
+
+func (t *Token) toString() string {
+	return fmt.Sprintf("%v %v %v", t.tokenType, t.lexeme, t.literal)
+}
